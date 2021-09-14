62b9dbf4fdd2defd1d9124e03cfa1323cf35fdc4
형준혁 <gudwnsgur12@naver.com>
2021-09-15 01:31:40 +0900

    [ADD] JPA study - 엔티티 클래스 개발 [ Address(값 타입) ]

--- src/main/java/com/study/jpaStudy/domain/Address.java
+++ src/main/java/com/study/jpaStudy/domain/Address.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import javax.persistence.Embeddable;
 
+// 값 타입
 @Embeddable  // 어딘가 내장될 수 있다.
 @Getter
 public class Address {
@@ -12,4 +13,14 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    // 기본 생성자 필요
+    protected Address() {}
+
+    // 값을 변경할 수 없게 설계 되어야 한다.
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
