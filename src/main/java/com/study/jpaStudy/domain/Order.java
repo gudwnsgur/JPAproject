6cac04390acf14ec9b4dba042fb89264f43ad7e9
형준혁 <gudwnsgur12@naver.com>
2021-09-15 13:28:05 +0900

    [ADD] JPA study - 연관관계 메서드 구현

--- src/main/java/com/study/jpaStudy/domain/Order.java
+++ src/main/java/com/study/jpaStudy/domain/Order.java
@@ -24,10 +24,16 @@ public class Order {
     @JoinColumn(name = "member_id") // 매핑을 무엇으로 할것인가
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
+    // persist(orderItemA)
+    // persist(orderItemB)
+    // persist(orderItemC)
+    // persist(order)
+    // cascade : 위의 코드를 아래로 줄일 수 있다.
+    // presist(order)
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -36,4 +42,28 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태 [ORDER, CANCEL]
+
+    // == 연관관계 메서드 == //
+    public void setMember(Member member) {
+//        Member member = new Member();
+//        Order order = new Order();
+//
+//        member.getOrders().add(order);
+//        order.setMember(member);
+
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
 }
