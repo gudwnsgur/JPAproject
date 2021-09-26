61f93e7390a5d2973be066638bc2d9b744ceb202
형준혁 <gudwnsgur12@naver.com>
2021-09-26 16:55:28 +0900

    [ADD] JPA study - Item Service

---
+++ src/main/java/com/study/jpaStudy/service/ItemService.java
@@ -0,0 +1,31 @@
+package com.study.jpaStudy.service;
+
+import com.study.jpaStudy.domain.item.Item;
+import com.study.jpaStudy.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+// Item Repository 에 단순 위임만 하게 된다.
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
