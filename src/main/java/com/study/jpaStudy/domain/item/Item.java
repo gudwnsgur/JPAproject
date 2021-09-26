24c423b56c593beafdc343aacad19401e1f0d5be
형준혁 <gudwnsgur12@naver.com>
2021-09-26 16:42:32 +0900

    [ADD] JPA study - Item 비즈니스 로직 추가(stock 관리)

--- src/main/java/com/study/jpaStudy/domain/item/Item.java
+++ src/main/java/com/study/jpaStudy/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.study.jpaStudy.domain.item;
 
 import com.study.jpaStudy.domain.Category;
+import com.study.jpaStudy.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,26 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // == 비즈니스 로직 == //
+
+    // Setter를 통한 변경이 아닌 로직을 통해서 변경시켜라!
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0 ) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
