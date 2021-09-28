b5337c5627a77abceba42bf2e3dd1abb8a108456
형준혁 <gudwnsgur12@naver.com>
2021-09-28 22:10:22 +0900

    [ADD] JPA study - 회원 등록 controller

---
+++ src/main/java/com/study/jpaStudy/controller/MemberController.java
@@ -0,0 +1,43 @@
+package com.study.jpaStudy.controller;
+
+import com.study.jpaStudy.domain.Address;
+import com.study.jpaStudy.domain.Member;
+import com.study.jpaStudy.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+        // vaild 이후의 result에 오류가 넘어간다.
+        if(result.hasErrors()) return "members/createMemberForm";
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
